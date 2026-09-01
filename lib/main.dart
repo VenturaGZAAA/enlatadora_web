645c72402c5198a101b29ea9b9db553e24c9045a
sousuke sagara <francis_4803@hotmail.com>
2026-08-31 23:51:18 -0600

    List of mock data.

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,50 @@
-import 'package:enlatadora_web/layout/main_layout.dart';
-import 'package:enlatadora_web/providers/mqtt_riverpod.dart';
-import 'package:enlatadora_web/screens/mqtt_thing_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mqtt_client/mqtt_client.dart';
+import 'package:enlatadora_web/screens/home_page.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
-  runApp(ProviderScope(child: const MyApp()));
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
-  // This widget is the root of your application.
+  final TextTheme textTheme = TextTheme(
+    titleLarge: GoogleFonts.caveat(fontSize: 32.0, fontWeight: FontWeight.bold),
+    headlineLarge: GoogleFonts.caveat(
+      fontSize: 25.0,
+      fontWeight: FontWeight.bold,
+    ),
+    bodyMedium: GoogleFonts.arima(fontSize: 16.0),
+  );
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'Dashboard',
-      home: const MyHomePage(rootTopico: 'home/test/'),
-    );
-  }
-}
-
-class MyHomePage extends MqttThingScreen {
-  const MyHomePage({super.key, required super.rootTopico})
-    : super(name: "home");
-
-  @override
-  MqttThingScreenState<MqttThingScreen> createState() {
-    return _MyHomePageState();
-  }
-}
-
-class _MyHomePageState extends MqttThingScreenState<MyHomePage> {
-  late final String ledTopic = "${widget.rootTopico}led";
-  late final String readTopic = "${widget.rootTopico}read";
-
-  String read = "0";
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  void mate() async {
-    await ref.read(mqttProvider.future);
-    await mqttNotifier.connect();
-    mqttNotifier.subscribe(readTopic, (content) {
-      setState(() {
-        read = content;
-      });
-
-    }, qos: MqttQos.atMostOnce);
-  }
-
-  @override
-  void subscribeToTopics() {
-    mate();
-
-  }
-
-  @override
-  void unsubscribeToTopics() {
-    mqttNotifier.unsubscribe(readTopic);
-    mqttNotifier.disconnect(manual: false);
-  }
-
-  @override
-  Widget buildBody(BuildContext context) {
-    return _body();
-  }
-
-  Widget _body() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Text("Lectura: $read"),
-        ElevatedButton(
-          onPressed: ()  {
-            // debugPrint("It's me dad!");
-            if (!mqttNotifier.isConnected()) {
-              ScaffoldMessenger.maybeOf(context)?.showSnackBar(
-                SnackBar(content: Text("Not connected bro")),
-              );
-            }
-            mqttNotifier.publish(ledTopic, "Hellooo");
-          },
-          child: Text(
-            "Press meee",
-          ),
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.light,
         ),
-      ],
+        textTheme: textTheme,
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark,
+        ),
+        textTheme: textTheme,
+      ),
+      themeMode: ThemeMode.system,
+      home: const HomePage(rootTopico: 'home/test/'),
     );
   }
 }
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,93 @@
+import 'package:enlatadora_web/providers/mqtt_riverpod.dart';
+import 'package:enlatadora_web/screens/mqtt_thing_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+
+class HomePage extends MqttThingScreen {
+  const HomePage({super.key, required super.rootTopico}) : super(name: "home");
+
+  @override
+  MqttThingScreenState<MqttThingScreen> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends MqttThingScreenState<HomePage> {
+  late final String ledTopic = "${widget.rootTopico}led";
+  late final String readTopic = "${widget.rootTopico}read";
+
+  static const int MAX_ITEMS = 50;
+
+  final List<MockData> reads = List.empty(growable: true);
+
+  void mate() async {
+    await ref.read(mqttProvider.future);
+    await mqttNotifier.connect();
+    mqttNotifier.subscribe(readTopic, (content) {
+      setState(() {
+        reads.add(MockData(DateTime.now(), content));
+        if (reads.length > MAX_ITEMS) {
+          reads.removeAt(0);
+        }
+      });
+    }, qos: MqttQos.atMostOnce);
+  }
+
+  @override
+  void subscribeToTopics() {
+    mate();
+  }
+
+  @override
+  void unsubscribeToTopics() {
+    mqttNotifier.unsubscribe(readTopic);
+    mqttNotifier.disconnect(manual: false);
+  }
+
+  @override
+  Widget buildBody(BuildContext context) {
+    return _body();
+  }
+
+  Widget _body() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+          child: ListView.builder(
+            reverse: true,
+            itemCount: reads.length,
+            itemBuilder: (ctx, index) => reads[index].view(),
+          ),
+        ),
+        SizedBox(height: 15),
+        ElevatedButton(
+          onPressed: () {
+            // debugPrint("It's me dad!");
+            if (!mqttNotifier.isConnected()) {
+              ScaffoldMessenger.maybeOf(
+                context,
+              )?.showSnackBar(SnackBar(content: Text("Not connected bro")));
+            }
+            mqttNotifier.publish(ledTopic, "Hellooo");
+          },
+          child: Text("Press meee"),
+        ),
+      ],
+    );
+  }
+}
+
+class MockData {
+  const MockData(this.time, this.message);
+
+  final DateTime time;
+  final String message;
+
+  Widget view() {
+    return ListTile(
+      title: Text(message),
+      subtitle: Text("${time.hour}-${time.minute}-${time.second}"),
+    );
+  }
+}
