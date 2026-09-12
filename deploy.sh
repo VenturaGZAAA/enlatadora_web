1125d15e18c3446c8ad8aba1a178e34a6dab4a87
sousuke sagara <francis_4803@hotmail.com>
2026-09-12 11:53:56 -0600

    Moved environment files into their own dir.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 rm -rf ./build/web
 
-flutter build web --release --dart-define-from-file=config_esp.json
+flutter build web --release --dart-define-from-file=environment/config_esp.json
 
 cd build/web || exit 1
 
